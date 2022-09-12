f188c5bb3a6b35e3f5ac8bd193c6481ce7ee541f
Tripsglamour <reeste555@gmail.com>
2022-09-12 16:40:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This program prints out singke digits of base 10 starting from 0
+ * followed by new line
+ * Return: Always 0 success
+ */
+
+int main(void);
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+		putchar(i)
+	putchar("\n");
+	return (0);
+}
+
