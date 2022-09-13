85260bfa87b9ebb5dfcd95890a26ba423e1716dd
Tripsglamour <reeste555@gmail.com>
2022-09-12 17:26:06 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - This program prints out singke digits of base 10 starting from 0
+ * main - This program prints out "single digits of base 10 starting from "0
  * followed by new line
  * Return: Always 0 success
  */
@@ -13,8 +13,9 @@ int main(void)
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
-
-		putchar(d)
-	putchar("\n");
+	{
+		putchar(d);
+	}
+	putchar('\n');
 	return (0);
 }
