a783715dc3a05c06ea33c9224ac364f6272cb62b
Tripsglamour <reeste555@gmail.com>
2022-09-12 17:46:08 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints lower case alpabets in reverse
+ * followed by a new line.
+ * Return: Always 0 success
+ */
+
+int main(void);
+{
+	char ch;
+
+	for (cha = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
