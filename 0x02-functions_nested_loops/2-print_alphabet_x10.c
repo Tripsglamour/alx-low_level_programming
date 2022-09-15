7cbba6219cfdf8f6b07f9120e742b1d76b490992
Tripsglamour <reeste555@gmail.com>
2022-09-15 09:53:08 -0700

    2.10x-alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main h"
+
+/**
+ * print_alphabet_x10 - use only "putchar" twice
+ *
+ * Return : 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char a_z;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (a_z = 'a'; a_z <= 'z'; a_z++)
+		{
+			_putchar(a_z);
+		}
+		_putchar('\n');
+	}
+}
+
