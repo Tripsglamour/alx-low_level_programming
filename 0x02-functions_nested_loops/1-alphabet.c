bdda1d995f6e7fef26390173e7847d2e2a573c70
Tripsglamour <reeste555@gmail.com>
2022-09-14 21:44:36 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - A funcrion that prints alphabet in lower case
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return (0);
+
