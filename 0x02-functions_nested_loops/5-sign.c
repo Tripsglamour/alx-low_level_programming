b6255593ab4ccf4bf9dcd51536ac1a6148761bad
Tripsglamour <reeste555@gmail.com>
2022-09-15 18:56:13 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.c"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * with n being an interger
+ *
+ * Returns: 0,1,-1 and sign of n
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (i);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
