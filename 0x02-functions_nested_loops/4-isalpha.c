83441dbda0228dc92ac77dfc485dc704ebc42f42
Tripsglamour <reeste555@gmail.com>
2022-09-15 18:39:21 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic characters.
+ *
+ * with c being an interger
+ *
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if  ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
